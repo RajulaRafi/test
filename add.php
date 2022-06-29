1142776a7faaf817b297724e11c5abcc531b01fc
Rajula <krajula@wallpostdev.com>
2022-06-29 10:51:32 +0530

    new commit

--- add.php
+++ add.php
@@ -3,6 +3,12 @@ $x=10;
 $y=3;
 $s= $x+$y;
 print_r("sum is");
+print_r("sum is");
+print_r("sum is");
+print_r("sum is");
+print_r("sum is");
+print_r("sum is:");
+
 echo $s;
 print_r(". All understood addition of two numbers");
 
